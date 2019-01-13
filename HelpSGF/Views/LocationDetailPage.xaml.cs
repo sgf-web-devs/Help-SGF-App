a472447bf39adb9efed53b0de618b6bf712f9c54
Myke Bates <contact@mykebates.com>
2019-01-13 14:55:43 -0600

    Start of location detail contact list

--- HelpSGF/Views/LocationDetailPage.xaml.cs
+++ HelpSGF/Views/LocationDetailPage.xaml.cs
@@ -81,6 +81,47 @@ namespace HelpSGF.Views
 
             // Address
             AddressLabel.Text = viewModel.Location.FormattedAddress;
+
+            // Contact List
+            foreach (var contact in viewModel.Location.contacts)
+            {
+                var contactType = contact.ContactType;
+                var imageIcon = "call.png";
+                if(contactType == "email") { imageIcon = "email.png"; }
+
+                var contentStackLayout = new StackLayout { Padding = new Thickness { Left = 20, Top = 20 } };
+                var lineStackLayout = new StackLayout { Padding = new Thickness { Left = 20, Right = 20 }, Margin = new Thickness { Top = 10 } };
+                var boxView = new BoxView
+                {
+                    BackgroundColor = Color.FromHex("DADADA"),
+                    HeightRequest = 2,
+                    VerticalOptions = LayoutOptions.Start,
+                    HorizontalOptions = LayoutOptions.FillAndExpand,
+                    Margin = 0
+                };
+
+                lineStackLayout.Children.Add(boxView);
+
+                var grid = new Grid();
+                grid.ColumnDefinitions.Add(new ColumnDefinition { Width = 30 });
+                grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+                var image = new Image { Source = imageIcon };
+                var textStackLayout = new StackLayout { Margin = new Thickness { Left = 10 } };
+                var title = new Label { FontSize = 16, FontAttributes = FontAttributes.Bold, Text = contact.ContactType };
+                var description = new Label { FontSize = 17, TextColor = Color.FromHex("813C27"), Text = contact.ContactData };
+
+                textStackLayout.Children.Add(title);
+                textStackLayout.Children.Add(description);
+
+                grid.Children.Add(image, 0, 0);
+                grid.Children.Add(textStackLayout, 1, 0);
+
+                contentStackLayout.Children.Add(grid);
+
+                ContactList.Children.Add(contentStackLayout);
+                ContactList.Children.Add(lineStackLayout);
+            }
         }
     }
 }
